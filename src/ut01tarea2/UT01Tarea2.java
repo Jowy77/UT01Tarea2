f8f5ec1bdaaf26950fd17a496272390ed6b29502
2damb <2damb@informatica.edu>
2023-10-09 19:28:48 +0100

    queda curro pa

---
+++ src/ut01tarea2/UT01Tarea2.java
@@ -0,0 +1,17 @@
+
+package ut01tarea2;
+
+import interfaces.datosInterface;
+
+
+public class UT01Tarea2 {
+
+   
+    public static void main(String[] args) {
+        
+        datosInterface app = new datosInterface();
+        
+        app.setVisible(true);
+    }
+    
+}
